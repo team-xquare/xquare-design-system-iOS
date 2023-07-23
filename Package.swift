7ca88b7e1aea79f09cb2daea14eb4a4604671d0d
juyeong525 <juyeongno1@naver.com>
2023-07-23 22:57:03 +0900

    ⚙️ :: 디자인시스템 세팅

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "xquare-design-system-iOS",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "xquare-design-system-iOS",
+            targets: ["xquare-design-system-iOS"]),
+    ],
+    targets: [
+        .target(
+            name: "xquare-design-system-iOS",
+            path: "xquare-design-system-iOS"
+        )
+    ]
+)
---
+++ xquare-design-system-iOS/Sources/Color/Error.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+public extension Color {
+    struct Error { }
+}
+
+public extension Color.Error {
+    static let error50: Color = Color("Error 50")
+}
