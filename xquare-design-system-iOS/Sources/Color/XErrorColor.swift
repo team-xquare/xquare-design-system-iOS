da1e5570eab3ada1a01231d047ad078c33a18918
juyeong525 <juyeongno1@naver.com>
2023-07-24 21:10:57 +0900

    ⚙️ :: XErrorColor 추가

--- xquare-design-system-iOS/Sources/Color/XErrorColor.swift
+++ xquare-design-system-iOS/Sources/Color/XErrorColor.swift
@@ -5,8 +5,22 @@ public extension Color {
 }
 
 public extension Color.Error {
+    private static let error0: Color = Color(type: .error, 0)
+    private static let error10: Color = Color(type: .error, 10)
+    private static let error20: Color = Color(type: .error, 20)
+    private static let error30: Color = Color(type: .error, 30)
     private static let error40: Color = Color(type: .error, 40)
+    private static let error50: Color = Color(type: .error, 50)
     private static let error60: Color = Color(type: .error, 60)
+    private static let error70: Color = Color(type: .error, 70)
+    private static let error80: Color = Color(type: .error, 80)
+    private static let error90: Color = Color(type: .error, 90)
+    private static let error95: Color = Color(type: .error, 95)
+    private static let error99: Color = Color(type: .error, 99)
+    private static let error100: Color = Color(type: .error, 100)
 
     static let error: Color = error60.darkModeColor(error40)
+    static let onError: Color = error100.darkModeColor(error100)
+    static let errorContainer: Color = error90.darkModeColor(error30)
+    static let onErrorContainer: Color = error10.darkModeColor(error90)
 }
