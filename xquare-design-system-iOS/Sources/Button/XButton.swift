fb1075d63294fbcdb94b9b36e61e560dcd935df8
juyeong525 <juyeongno1@naver.com>
2023-07-27 21:59:22 +0900

    ⚡️ :: XButton 추가

---
+++ xquare-design-system-iOS/Sources/Button/XButton.swift
@@ -0,0 +1,102 @@
+import SwiftUI
+
+public struct XButton: View {
+
+    @Binding var isDisabled: Bool
+    var image: Image?
+    var text: String
+    var action: () -> ()
+    var type: XButtonType
+
+    public init(
+        isDisabled: Binding<Bool> = .constant(true),
+        image: Image? = nil,
+        text: String,
+        action: @escaping () -> (),
+        type: XButtonType = .filled
+    ) {
+        self._isDisabled = isDisabled
+        self.image = image
+        self.text = text
+        self.action = action
+        self.type = type
+    }
+
+    public var body: some View {
+        switch type {
+        case .filled:
+            Button(action: action) {
+                HStack(spacing: 8) {
+                    if let image = image {
+                        image
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                            .foregroundColor( .Primary.onPrimary)
+                    }
+                    Text(text)
+                }
+            }
+            .buttonStyle(XButtonStyle(type: .filled))
+            .disabled(isDisabled)
+        case .outlined:
+            Button(action: action) {
+                HStack(spacing: 8) {
+                    if let image = image {
+                        image
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(isDisabled
+                                             ? .Primary.primary.opacity(0.4)
+                                             : .Primary.primary)
+                    }
+                    Text(text)
+                }
+            }
+            .buttonStyle(XButtonStyle(type: .outlined))
+            .disabled(isDisabled)
+        case .text:
+            Button(action: action) {
+                HStack(spacing: 8) {
+                    if let image = image {
+                        image
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(isDisabled
+                                             ? .Neutral.onSurface.opacity(0.4)
+                                             : .Neutral.onSurface)
+                    }
+                    Text(text)
+                }
+            }
+            .buttonStyle(XButtonStyle(type: .text))
+            .disabled(isDisabled)
+        }
+    }
+}
+// MARK: Preview
+struct XButton_Preview: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            Spacer()
+            XButton(isDisabled: .constant(false), image: Image(systemName: "pencil.circle.fill"), text: "Enabled", action: { print("sadfq") }, type: .text)
+            XButton(isDisabled: .constant(true), image: Image(systemName: "pencil.circle.fill"), text: "Enabled", action: { print("sadfq") }, type: .text)
+            XButton(isDisabled: .constant(false), image: Image(systemName: "pencil.circle.fill"), text: "Enabled", action: { print("sadfq") }, type: .outlined)
+            XButton(isDisabled: .constant(true), image: Image(systemName: "pencil.circle.fill"), text: "Enabled", action: { print("sadfq") }, type: .outlined)
+            XButton(isDisabled: .constant(false), image: Image(systemName: "pencil.circle.fill"), text: "Enabled", action: { print("sadfq") }, type: .filled)
+            XButton(isDisabled: .constant(true), image: Image(systemName: "pencil.circle.fill"), text: "Enabled", action: { print("sadfq") }, type: .filled)
+            VStack {
+                HStack{
+                    
+                    XButton(isDisabled: .constant(false), text: "Enabled", action: { print("sadfq") }, type: .text)
+                    XButton(isDisabled: .constant(true), text: "Enabled", action: { print("sadfq") }, type: .text)
+                    XButton(isDisabled: .constant(false), text: "Enabled", action: { print("sadfq") }, type: .outlined)                }
+                HStack {
+                    XButton(isDisabled: .constant(true), text: "Enabled", action: { print("sadfq") }, type: .outlined)
+                    XButton(isDisabled: .constant(false), text: "Enabled", action: { print("sadfq") }, type: .filled)
+                    XButton(isDisabled: .constant(true), text: "Enabled", action: { print("sadfq") }, type: .filled)
+                }
+            }
+            Spacer()
+        }
+    }
+}
