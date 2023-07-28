cb0d3c095dfdd39102f43953a51a1b9a1d312b5a
juyeong525 <juyeongno1@naver.com>
2023-07-28 22:05:07 +0900

    ⚡️ :: ButtonPlaygroundView에 모든 버튼 추가

--- xquare-design-system-iOS/Sources/Playground/Views/ButtonPlaygroundView.swift
+++ xquare-design-system-iOS/Sources/Playground/Views/ButtonPlaygroundView.swift
@@ -5,54 +5,158 @@ struct ButtonPlaygroundView: View {
     var body: some View {
         ScrollView {
             VStack {
+                Spacer()
                 Toggle(
                     isOn: $isDisable,
                     label: {
                         Label("isDisable", systemImage: "xmark")
                     }
                 )
-
-                XButton(
-                    "Filled 버튼",
-                    icon: .filledHeart,
-                    type: .filled,
-                    action: {}
-                )
-                .disabled(isDisable)
-
-                XButton(
-                    "Outlined 버튼",
-                    icon: .filledHeart,
-                    type: .outlined,
-                    action: {}
-                )
-                .disabled(isDisable)
-
-                XButton(
-                    "Text 버튼",
-                    icon: .filledHeart,
-                    type: .text,
-                    action: {}
-                )
-                .disabled(isDisable)
-                XIconButton(
-                    icon: .filledHeart,
-                    type: .filled,
-                    action: {}
-                )
-                .disabled(isDisable)
-                XIconButton(
-                    icon: .filledHeart,
-                    type: .outlined,
-                    action: {}
-                )
-                .disabled(isDisable)
-                XIconButton(
-                    icon: .filledHeart,
-                    type: .icon,
-                    action: {}
-                )
-                .disabled(isDisable)
+                HStack {
+                    VStack {
+                        Text("XButton")
+                        XButton(
+                            "Filled 버튼",
+                            icon: .filledHeart,
+                            type: .filled,
+                            action: {}
+                        )
+                        .disabled(isDisable)
+                        
+                        XButton(
+                            "Outlined 버튼",
+                            icon: .filledHeart,
+                            type: .outlined,
+                            action: {}
+                        )
+                        .disabled(isDisable)
+                        
+                        XButton(
+                            "Text 버튼",
+                            icon: .filledHeart,
+                            type: .text,
+                            action: {}
+                        )
+                        .disabled(isDisable)
+                    }
+                    VStack {
+                        Text("XIconButton")
+                        XIconButton(
+                            icon: .filledHeart,
+                            type: .filled,
+                            action: {}
+                        )
+                        .disabled(isDisable)
+                        XIconButton(
+                            icon: .filledHeart,
+                            type: .outlined,
+                            action: {}
+                        )
+                        .disabled(isDisable)
+                        XIconButton(
+                            icon: .filledHeart,
+                            type: .icon,
+                            action: {}
+                        )
+                        .disabled(isDisable)
+                    }
+                }
+                VStack {
+                    Text("XChipButton")
+                    HStack {
+                        VStack {
+                            XChipButton(
+                                "Filled",
+                                type: .filled,
+                                action: { }
+                            )
+                            .disabled(isDisable)
+                            XChipButton(
+                                "Filled",
+                                rightIcon: .filledGear,
+                                type: .filled,
+                                action: { }
+                            )
+                            .disabled(isDisable)
+                            XChipButton(
+                                "Filled",
+                                leftIcon: .filledGear,
+                                type: .filled,
+                                action: { }
+                            )
+                            .disabled(isDisable)
+                            XChipButton(
+                                "Filled",
+                                leftIcon: .filledGear,
+                                rightIcon: .filledGear,
+                                type: .filled,
+                                action: { }
+                            )
+                            .disabled(isDisable)
+                        }
+                        VStack {
+                            XChipButton(
+                                "Outlined",
+                                type: .outlined,
+                                action: { }
+                            )
+                            .disabled(isDisable)
+                            XChipButton(
+                                "Outlined",
+                                rightIcon: .filledGear,
+                                type: .outlined,
+                                action: { }
+                            )
+                            .disabled(isDisable)
+                            XChipButton(
+                                "Outlined",
+                                leftIcon: .filledGear,
+                                type: .outlined,
+                                action: { }
+                            )
+                            .disabled(isDisable)
+                            XChipButton(
+                                "Outlined",
+                                leftIcon: .filledGear,
+                                rightIcon: .filledGear,
+                                type: .outlined,
+                                action: { }
+                            )
+                            .disabled(isDisable)
+                        }
+                    }
+                }
+                HStack {
+                    VStack {
+                        Text("XIconChipButton")
+                        XIconChipButton(
+                            icon: .filledGear,
+                            type: .filled,
+                            action: { }
+                        )
+                        .disabled(isDisable)
+                        XIconChipButton(
+                            icon: .filledGear,
+                            type: .outlined,
+                            action: { }
+                        )
+                        .disabled(isDisable)
+                    }
+                    VStack {
+                        Text("XFloatingActionButton")
+                        XFloatingActionButton(
+                            icon: .filledMenu,
+                            action: { }
+                        )
+                        .disabled(isDisable)
+                        XFloatingActionButton(
+                            text: "Filled",
+                            icon: .filledMenu,
+                            action: { }
+                        )
+                        .disabled(isDisable)
+                    }
+                }
             }
         }
     }
